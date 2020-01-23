b771453ff40bfe41a6df8977fae2b2900c239ae0
Yacine Benzerga <benzerga.yacine@hotmail.com>
2020-01-23 01:56:42 -0500

    feat(batchProcessing) added Airflow spark job scheduler

---
+++ spark_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Add config files here:
+
+
+
+
+#Add bash flags here
+
+case $1 in 
+
+    --batch)
+
+    #Submit spark jobs
+    ;;
+
+    PATTERN_2)
+    STATEMENTS
+    ;;
+
+    PATTERN_N)
+    STATEMENTS
+    ;;
+esac
